079cb19611b9b39a4ac19682eac08217177590f8
Karbonforms <karbonforms@gmail.com>
2021-11-06 01:37:58 +0000

    init

---
+++ NeoPixelF7.h
@@ -0,0 +1,7 @@
+#ifndef MIDILIGHTSF7_NEOPIXELF7_H
+#define MIDILIGHTSF7_NEOPIXELF7_H
+
+void NeoPixelF7_init();
+void NeoPixelF7_show(const uint32_t* ptr, uint32_t num_pixels);
+
+#endif //MIDILIGHTSF7_NEOPIXELF7_H
